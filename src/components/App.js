7e8a9db61741545bdadae79f3e460eb5859c9bb2
przbadu <pushparaj.badu@gmail.com>
2017-08-15 11:54:42 +0545

    Refactor: * update project structor * Add components directory for components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Build successful, now we can proceed furter
+      and build the app.
+    </div>
+  );
+}
+
+export default App;
